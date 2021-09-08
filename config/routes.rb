11eb5bd79d113976dc6226d858a6af6d11c81405
Ailin Soon <soonailin@gmail.com>
2021-09-09 01:00:48 +0800

    chapter_13

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: :edit
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :microposts, only: [:create, :destroy]
+
 
   get "/login", to: "session#new"
   post "/login", to: "session#create"
